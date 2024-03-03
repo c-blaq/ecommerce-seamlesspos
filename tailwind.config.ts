012f94cffdf84f6a58ec3681d831272309e6df7b
Fathi <babayejuadesina@gmail.com>
2024-03-03 19:14:48 +0100

    chore: extend theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          DEFAULT: "#1B42E4",
+        },
+        secondary: {
+          DEFAULT: "blue",
+        },
+        textGray: {
+          100: "#E5E7EB",
+        },
       },
     },
   },
