19ac54a3dd96c331d7a2ea412111e161b4def620
Fathi <babayejuadesina@gmail.com>
2024-03-04 00:16:31 +0100

    style: about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import AboutUS from "@/components/home/About";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <div className="max-w-screen-2xl mx-auto">
+        <AboutUS />
+      </div>
     </>
   );
 }
