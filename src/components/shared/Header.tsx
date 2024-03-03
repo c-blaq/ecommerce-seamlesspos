f26bd104ce3b9c89a738053326b0bdf6bcdf7435
Fathi <babayejuadesina@gmail.com>
2024-03-04 00:02:48 +0100

    chore: improve responsiveness

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -5,8 +5,8 @@ import { IoBagOutline } from "react-icons/io5";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-white items-center h-16 lg:h-20 px-5">
-      <div className="flex justify-between items-center gap-5 h-full w-full max-w-screen-2xl mx-auto py-4">
+    <header className="bg-transparent fixed z-10 left-0 right-0 top-5 items-center h-16 px-5">
+      <div className="flex justify-between items-center gap-5 h-full w-full max-w-screen-2xl mx-auto sm:rounded-full backdrop-blur-md sm:px-5 py-4">
         <h1 className="text-lg sm:text-xl font-semibold transition duration-300">
           <Link href="/">
             <Image
@@ -22,12 +22,12 @@ const Header: React.FC = () => {
         </h1>
 
         <div className="flex items-center gap-4">
-          <button className="hover:text-gray-500 transition duration-300">
+          <button className="hover:text-gray-500 text-primary-dark transition duration-300">
             <IoBagOutline className="text-xl sm:text-2xl" />
           </button>
           <Link
             href="/"
-            className="bg-primary text-textGray-100 px-3 py-1 sm-px-5 sm:py-2 rounded hover:bg-primary-dark transition duration-300"
+            className="bg-primary text-textGray-100 px-3 py-1 sm:px-5 sm:py-2 rounded hover:bg-primary-dark transition duration-300"
           >
             Login
           </Link>
