a96325dcd9ed9413dda8323b799596d7855e1294
Fathi <babayejuadesina@gmail.com>
2024-03-04 20:43:30 +0100

    chore: improve responsiveness

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -56,7 +56,7 @@ const Product = ({ params }: { params: { slug: string } }) => {
       </Link>
 
       {product ? (
-        <div className="flex  h-full flex-col lg:flex-row justify-between gap-5 items-center">
+        <div className="flex -mx-5 lg:mx-0 h-full flex-col lg:flex-row justify-between gap-5 items-center">
           <div className="min-h-full w-full bg-textGray-100 flex-1 ">
             <Image
               src={""}
@@ -67,23 +67,28 @@ const Product = ({ params }: { params: { slug: string } }) => {
             />
           </div>
 
-          <div className="flex-1 mt-5 lg:mt-0">
+          <div className="flex-1 mt-5 lg:mt-0 px-5 lg:px-0">
             <div className="flex justify-between items-center">
               <h2 className="text-xl font-semibold">{product?.name}</h2>
               <span className="text-textBlack text-lg">{product?.price}</span>
             </div>
-            <div className="py-5">{product?.description}</div>
+            <div className="py-5 text-sm lg:text-base leading-loose">
+              {product?.description}
+            </div>
 
             {product?.additionalDetails && (
               <div>
-                <p className="text-lg font-semibold mt-4">
+                <h3 className="lg:text-lg font-semibold mt-4">
                   Additional Details:
-                </p>
-                <ul className="mt-2 grid grid-cols-2 gap-3">
+                </h3>
+                <ul className="mt-2 grid sm:grid-cols-2 gap-3">
                   {Object.entries(product?.additionalDetails).map(
                     ([key, value]) => (
                       <li key={key}>
-                        <span className="font-semibold">{key}:</span> {value}
+                        <span className="font-semibold text-sm lg:text-base">
+                          {key}:
+                        </span>{" "}
+                        {value}
                       </li>
                     )
                   )}
@@ -91,7 +96,7 @@ const Product = ({ params }: { params: { slug: string } }) => {
               </div>
             )}
 
-            <div className="flex gap-4 mt-5 md:mt-8">
+            <div className="flex gap-4 mt-8 md:mt-10">
               <button className="text-sm h-10 bg-primary hover:bg-primary-dark text-white flex-1 rounded transition duration-300">
                 Buy Now
               </button>
